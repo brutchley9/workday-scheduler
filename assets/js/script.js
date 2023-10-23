8f2b57bf96797a18629ba5beff531157c2381f4f
brutchley <bhutchison0901@yahoo.com>
2023-10-23 18:18:17 -0500

    duplicated my functions for saving text values to local storage for 9am, then created new functions for each timeslot, calling each of these within jQuery function so that page loads BEFORE my functions are called

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,16 @@ $('#currentDay').text("Today's Date: " + (today.format('MM/DD/YYYY')));
 
 
 var buttonNine = document.getElementById("9am-todo");
+var buttonTen = document.getElementById("10am-todo");
+var buttonEleven = document.getElementById("11am-todo");
+var buttonNoon = document.getElementById("12pm-todo");
+var buttonThirteen = document.getElementById("1pm-todo");
+var buttonFourteen = document.getElementById("2pm-todo");
+var buttonFifteen = document.getElementById("3pm-todo");
+var buttonSixteen = document.getElementById("4pm-todo");
+var buttonSeventeen = document.getElementById("5pm-todo");
+
+
 
 buttonNine.addEventListener("click", function(event) {
   event.preventDefault();
@@ -20,11 +30,158 @@ function renderTodoNineText() {
   buttonNine.value = todoNine
 }
 
+buttonTen.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var todoTen = document.getElementById("todoTen").value;
+
+  localStorage.setItem("10am-todo", todoTen);
+  renderTodoTenText();
+
+
+});
+
+function renderTodoTenText() {
+  var todoTen = localStorage.getItem("10am-todo");
+  buttonTen.value = todoTen
+}
+
+buttonEleven.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var todoEleven = document.getElementById("todoEleven").value;
+
+  localStorage.setItem("11am-todo", todoEleven);
+  renderTodoElevenText();
+
+
+});
+
+function renderTodoElevenText() {
+  var todoEleven = localStorage.getItem("11am-todo");
+  buttonEleven.value = todoEleven
+}
+
+buttonNoon.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var todoNoon = document.getElementById("todoNoon").value;
+
+  localStorage.setItem("12pm-todo", todoNoon);
+  renderTodoNoonText();
+
+
+});
+
+function renderTodoNoonText() {
+  var todoNoon = localStorage.getItem("12pm-todo");
+  buttonNoon.value = todoNoon
+}
+
+buttonThirteen.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var todoThirteen = document.getElementById("todoThirteen").value;
+
+  localStorage.setItem("1pm-todo", todoThirteen);
+  renderTodoThirteenText();
+
+
+});
+
+function renderTodoThirteenText() {
+  var todoThirteen = localStorage.getItem("1pm-todo");
+  buttonThirteen.value = todoThirteen
+}
+
+buttonFourteen.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var todoFourteen = document.getElementById("todoFourteen").value;
+
+  localStorage.setItem("2pm-todo", todoFourteen);
+  renderTodoFourteenText();
+
+
+});
+
+function renderTodoFourteenText() {
+  var todoFourteen = localStorage.getItem("2pm-todo");
+  buttonFourteen.value = todoFourteen
+}
+
+buttonFifteen.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var todoFifteen = document.getElementById("todoFifteen").value;
+
+  localStorage.setItem("3pm-todo", todoFifteen);
+  renderTodoFifteenText();
+
+
+});
+
+function renderTodoFifteenText() {
+  var todoFifteen = localStorage.getItem("3pm-todo");
+  buttonFifteen.value = todoFifteen
+}
+
+buttonSixteen.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var todoSixteen = document.getElementById("todoSixteen").value;
+
+  localStorage.setItem("4pm-todo", todoSixteen);
+  renderTodoSixteenText();
+
+
+});
+
+function renderTodoSixteenText() {
+  var todoSixteen = localStorage.getItem("4pm-todo");
+  buttonSixteen.value = todoSixteen
+}
+
+buttonSeventeen.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var todoSeventeen = document.getElementById("todoSeventeen").value;
+
+  localStorage.setItem("5pm-todo", todoSeventeen);
+  renderTodoSeventeenText();
+
+
+});
+
+function renderTodoSeventeenText() {
+  var todoSeventeen = localStorage.getItem("5pm-todo");
+  buttonSeventeen.value = todoSeventeen
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
   renderTodoNineText();
+  renderTodoTenText();
+  renderTodoElevenText();
+  renderTodoNoonText();
+  renderTodoThirteenText();
+  renderTodoFourteenText();
+  renderTodoFifteenText();
+  renderTodoSixteenText();
+  renderTodoSeventeenText();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
